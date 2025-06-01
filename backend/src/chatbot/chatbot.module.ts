a15e400ba273189e2df1bfab8c9e7455ad43f67f
Seu Nome <seu-email@exemplo.com>
2025-05-31 21:14:34 -0300

    Fix: Chatbot agora usa StandingsService para calcular tabela dinamicamente baseado nos jogos finalizados

--- backend/src/chatbot/chatbot.module.ts
+++ backend/src/chatbot/chatbot.module.ts
@@ -14,6 +14,7 @@ import { Round } from '../entities/round.entity';
 import { Goal } from '../entities/goal.entity';
 import { Channel } from '../entities/channel.entity';
 import { UsersModule } from '../modules/users/users.module';
+import { StandingsService } from '../modules/standings/standings.service';
 
 @Module({
   imports: [
@@ -35,6 +36,7 @@ import { UsersModule } from '../modules/users/users.module';
     OpenAIService,
     EvolutionService,
     FootballDataService,
+    StandingsService,
   ],
   exports: [ChatbotService],
 })
