9cca3dcf081598deeb2767ca2d64d2a5623fb5d3
kmiza27 <kmiza27@gmail.com>
2025-06-03 18:03:13 -0300

    Ida e volta

--- backend/src/modules/matches/matches.module.ts
+++ backend/src/modules/matches/matches.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchesService } from './matches.service';
 import { MatchesController } from './matches.controller';
-import { Match, Team, Competition, Round, MatchBroadcast, Channel } from '../../entities';
+import { Match, Team, Competition, Round, MatchBroadcast, Channel, Stadium } from '../../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Match, Team, Competition, Round, MatchBroadcast, Channel])],
+  imports: [TypeOrmModule.forFeature([Match, Team, Competition, Round, MatchBroadcast, Channel, Stadium])],
   controllers: [MatchesController],
   providers: [MatchesService],
   exports: [MatchesService]
