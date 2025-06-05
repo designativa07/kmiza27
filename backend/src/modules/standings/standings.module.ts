d9ee16c8580e0d2920e0fefa78a868be700cb4db
kmiza27 <kmiza27@gmail.com>
2025-06-05 18:16:45 -0300

    fix: adiciona Team repository ao StandingsModule

--- backend/src/modules/standings/standings.module.ts
+++ backend/src/modules/standings/standings.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StandingsService } from './standings.service';
 import { StandingsController } from './standings.controller';
-import { CompetitionTeam, Match, Competition } from '../../entities';
+import { CompetitionTeam, Match, Competition, Team } from '../../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CompetitionTeam, Match, Competition])],
+  imports: [TypeOrmModule.forFeature([CompetitionTeam, Match, Competition, Team])],
   controllers: [StandingsController],
   providers: [StandingsService],
   exports: [StandingsService]
