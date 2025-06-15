2001858b8df0120100a80c9993f6f4c4fb294e0b
Seu Nome <seu-email@exemplo.com>
2025-06-15 12:39:21 -0300

    CORREOCOES ESTADIOS

---
+++ futepedia-frontend/src/types/stadium.ts
@@ -0,0 +1,10 @@
+export interface Stadium {
+  id: number;
+  name: string;
+  city: string;
+  state: string;
+  country: string;
+  capacity: number;
+  latitude?: number;
+  longitude?: number;
+} 
