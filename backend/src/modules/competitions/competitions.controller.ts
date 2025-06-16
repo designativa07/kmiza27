499cee5cf9b930ca02c39263e88d4aa414c25b2a
kmiza27 <kmiza27@gmail.com>
2025-06-16 16:41:25 -0300

    url no formulario de logo da competicao

--- backend/src/modules/competitions/competitions.controller.ts
+++ backend/src/modules/competitions/competitions.controller.ts
@@ -83,8 +83,8 @@ export class CompetitionsController {
 
         if (!existsSync(uploadPath)) {
           mkdirSync(uploadPath, { recursive: true });
-        }
-        cb(null, uploadPath);
+            }
+          cb(null, uploadPath);
       },
       filename: (req, file, cb) => {
         const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
