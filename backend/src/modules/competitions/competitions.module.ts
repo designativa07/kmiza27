998cbef50181d6615d9d4e6f28b11d1e61112738
kmiza27 <kmiza27@gmail.com>
2025-07-30 13:25:10 -0300

    feat: Alteracoes gerais e deploy

--- backend/src/modules/competitions/competitions.module.ts
+++ backend/src/modules/competitions/competitions.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompetitionsService } from './competitions.service';
 import { CompetitionsController } from './competitions.controller';
 import { UploadService } from './upload.service';
-import { Competition, CompetitionTeam, Goal } from '../../entities';
+import { Competition, CompetitionTeam, Goal, Player } from '../../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Competition, CompetitionTeam, Goal])],
+  imports: [TypeOrmModule.forFeature([Competition, CompetitionTeam, Goal, Player])],
   controllers: [CompetitionsController],
   providers: [CompetitionsService, UploadService],
   exports: [CompetitionsService, UploadService]
--- backend/src/modules/standings/standings.module.ts
+++ backend/src/modules/standings/standings.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StandingsService } from './standings.service';
 import { StandingsController } from './standings.controller';
 import { CompetitionsService } from '../competitions/competitions.service';
-import { CompetitionTeam, Match, Competition, Team, Goal } from '../../entities';
+import { CompetitionTeam, Match, Competition, Team, Goal, Player } from '../../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CompetitionTeam, Match, Competition, Team, Goal])],
+  imports: [TypeOrmModule.forFeature([CompetitionTeam, Match, Competition, Team, Goal, Player])],
   controllers: [StandingsController],
   providers: [StandingsService, CompetitionsService],
   exports: [StandingsService]
